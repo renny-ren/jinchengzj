f4aacee423c92e708454f15ea31a84b163244f93
rennyallen <rennyallen@hotmail.com>
2017-04-30 11:16:18 +0800

    css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_local
+  
+  def set_local
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: 3..15
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
