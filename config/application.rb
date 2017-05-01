49377d9fdde05a195c5f61de5a719d4cdfe7ca43
rennyallen <rennyallen@hotmail.com>
2017-05-01 18:37:22 +0800

    install rspec to start test

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,12 @@ module Jczj
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = 'ch'
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
