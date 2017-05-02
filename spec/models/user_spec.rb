52e0b7890a3ba1aa70aa0ed4ffbead033322fcce
rennyallen <rennyallen@hotmail.com>
2017-05-02 22:49:17 +0800

    add test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe User do
+  it "is valid with username, password, captcha" do
+    expect(true).to be true
+  end
+  it "is invalid without a username"
+  it "is invalid without a password"
+  it "is invalid without a captcha"  
+end
