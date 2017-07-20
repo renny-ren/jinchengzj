f0a9bea6fb2959e923dc6de7fee02b6d70bdbed7
rennyallen <rennyallen@hotmail.com>
2017-07-20 12:03:45 +0800

    user rspec

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
 
   gem 'rspec-rails'
   gem 'factory_girl_rails'
+  # gem 'rspec-collection_matchers'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 describe User do
-  it "is valid with username, password, captcha" do
-    expect(true).to be true
+  it "is valid with username, password" do
+    user = User.new(
+      username: "Allen",
+      password: "password" )
+    expect(user).to be_valid
   end
-  it "is invalid without a username"
-  it "is invalid without a password"
-  it "is invalid without a captcha"  
+  
+  it "is invalid without a username" do
+    expect(User.new(username: nil)).to_not be_valid
+  end
+
+  it "is invalid without a password" do
+    expect(User.new(password: nil)).to_not be_valid
+  end
 end
