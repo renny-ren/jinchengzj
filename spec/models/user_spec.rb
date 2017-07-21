34a7030ae2b45c805697116abadcc73cfc391a02
rennyallen <rennyallen@hotmail.com>
2017-07-21 19:46:21 +0800

    factory test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 describe User do
   it "is valid with username, password" do
-    user = User.new(
-      username: "Allen",
-      password: "password" )
-    expect(user).to be_valid
+    expect(build(:user)).to be_valid
   end
-  
+
   it "is invalid without a username" do
     expect(User.new(username: nil)).to_not be_valid
   end
 
   it "is invalid without a password" do
     expect(User.new(password: nil)).to_not be_valid
+  end
+
+  it "is invalid with a duplicate username" do
+    create(:user, username: "allen")
+    user = build(:user, username: "allen")
+    expect(user).to_not be_valid
   end
 end
