1c413d75d0266357d16baa4d334b67115d765c18
rennyallen <rennyallen@hotmail.com>
2017-08-06 15:21:11 +0800

    update password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,30 @@ class ApplicationController < ActionController::Base
   before_action :set_local
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  KEYS_ARRAY = %i( username nickname email password password_confirmation current_password avatar signature )
+
   def set_local
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
+  def authenticate_user!(opts = {})
+    return if current_user
+    if turbolinks_app?
+      render plain: "401 Unauthorized", status: 401
+      return
+    end
+
+    store_location
+
+    super(opts)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
   protected
-
-  KEYS_ARRAY = %i( username nickname email password password_confirmation current_password avatar signature )
-
+  
   def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
