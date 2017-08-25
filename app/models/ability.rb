ac4a3c5b57b75d528b4809d1aca6d14c35df1cda
rennyallen <rennyallen@hotmail.com>
2017-08-25 23:59:21 +0800

    roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,24 @@ class Ability
     if user.is_admin?
     else
       can :read, :all
-      can [:edit, :update], Topic, user_id: user.id
-      can [:update, :destroy], Reply, user_id: user.id
-
+      roles_for_topic(user)
+      roles_for_reply(user)
     end
   end
+
+  protected
+
+  def roles_for_topic(user)
+    can :create, Topic
+    can :update, Topic, user_id: user.id
+    can :destroy, Topic do |topic|
+      topic.user_id == user.id && topic.replies_count == 0
+    end
+  end
+
+  def roles_for_reply(user)
+    can :create, Reply
+    can [:update, :destroy], Reply, user_id: user.id
+  end
+
 end
