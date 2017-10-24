68a97bc3df49ced7e316fe53e894d2bb066ee4a9
rennyallen <rennyallen@hotmail.com>
2017-10-24 23:02:02 +0800

    .

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,8 @@ class Notification < ActiveRecord::Base
   include Notifications::Model
 
   # Write your custom methods...
+  def self.notify_follow(user)
+    Notification.create notify_type: 'follow',
+    actor: current_user, user: user
+  end
 end
--- app/models/user/followable.rb
+++ app/models/user/followable.rb
@@ -5,5 +5,11 @@ class User
     included do
       action_store :follow, :user, counter_cache: 'followers_count', user_counter_cache: 'following_count'
     end
+
+    def follow_user(user)
+      return unless user
+      self.create_action(:follow, target: user)
+      Notification.notify_follow(user)
+    end
   end
 end
