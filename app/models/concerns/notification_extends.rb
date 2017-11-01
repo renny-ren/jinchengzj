60a74bbe227ea0944e3a5c09a33afb8a2b5e5c81
rennyallen <rennyallen@hotmail.com>
2017-11-01 16:15:03 +0800

    notification view

--- app/models/concerns/notification_extends.rb
+++ app/models/concerns/notification_extends.rb
@@ -1,8 +1,6 @@
 module NotificationExtends
   extend ActiveSupport::Concern
 
-  DEFAULT_AVATAR = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPAAAADwCAMAAAAJixmgAAAAFVBMVEWkpKSnp6eqqqq3t7fS0tLV1dXZ2dmshcKEAAAAtklEQVR4Ae3XsRGAAAjAQFRk/5HtqaTz5H+DlInvAQAAAAAAAAAAAAAAAAAAAACymiveO6o7BQsWLFiwYMGCBS8PFixYsGDBggULFixYsGDBggULFixYsGDBggULFixYsGDBc4IFCxYsWLBgwYIFC14ZfOeAPRQ8IliwYMGCBQsWLFiwYMGCBQsWLFiwYMGCBQsWLFiwYMGCBQsWLFiwYMGCBQv+JQAAAAAAAAAAAAAAAAAAAOAB4KJfdHmj+kwAAAAASUVORK5CYII='
-
   included do
     belongs_to :actor, class_name: Notifications.config.user_class
     belongs_to :user, class_name: Notifications.config.user_class
@@ -21,18 +19,6 @@ module NotificationExtends
   def actor_name
     return '' if self.actor.blank?
     self.actor.send(Notifications.config.user_name_method)
-  end
-
-  def actor_avatar_url
-    return DEFAULT_AVATAR if Notifications.config.user_avatar_url_method.blank?
-    return DEFAULT_AVATAR if self.actor.blank?
-    self.actor.send(Notifications.config.user_avatar_url_method)
-  end
-
-  def actor_profile_url
-    return nil if Notifications.config.user_profile_url_method.blank?
-    return nil if self.actor.blank?
-    self.actor.send(Notifications.config.user_profile_url_method)
   end
 
   module ClassMethods
