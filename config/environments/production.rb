001f1c60c66b7ba09437f1c0d49ebae5eb89c4d0
rennyallen <rennyallen@hotmail.com>
2018-01-06 13:53:44 +0800

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "jczj_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'jinchengzj.com' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.ym.163.com',
@@ -69,8 +71,6 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
