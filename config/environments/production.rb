e75ed76c57d43cca804b07e652406f2878a19d65
rennyallen <rennyallen@hotmail.com>
2018-01-06 14:55:22 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.default_url_options = { host: 'jinchengzj.com' }
+  config.action_mailer.default_options = { from: 'admin@jinchengzj.com' }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => 'utf-8'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.ym.163.com',
