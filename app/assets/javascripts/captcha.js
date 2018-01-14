0cbf241f6189d1dc6c46b8138cea22a487475519
rennyallen <rennyallen@hotmail.com>
2018-01-14 14:54:48 +0800

    fix refresh captcha

--- app/assets/javascripts/captcha.js
+++ app/assets/javascripts/captcha.js
@@ -1,6 +1,6 @@
 $(function(){
   $('.rucaptcha-image').click(function(){
     var num = Math.floor(Math.random()*1000000000);
-    $(this).attr('src', 'http://localhost:3000/rucaptcha/?' + num);
+    $(this).attr('src', '/rucaptcha/?' + num);
   });
 });
